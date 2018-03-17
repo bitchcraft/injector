766ecdc8874cf975d93f345a57562b2ed27e3bcf
maddrag0n <bxxxxcraft@gmail.com>
2018-03-17 09:23:53 +0100

    feat(injector-loader) webpack loader

---
+++ src/injector-loader/index.test.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+import injectorLoader from './';
+
+describe('injectorLoader()', () => {
+	const tests = [{
+		in: 'some: \'{{{placeholder}}}\'',
+		out: 'some: {{{placeholder}}}',
+	}, {
+		in: 'some: \'{{placeholder}}\';',
+		out: 'some: {{placeholder}};',
+	}, {
+		in: 'some: \'{{{placeholder}}}\' stuff;',
+		out: 'some: {{{placeholder}}} stuff;',
+	}, {
+		in: 'some: calc(\'{{{placeholder}}}\' - 15px);',
+		out: 'some: calc({{{placeholder}}} - 15px);',
+	}];
+
+	tests.forEach((test) => {
+		it(`('${test.in}') should return '${test.out}'`, () => expect(injectorLoader(test.in)).toBe(test.out));
+	});
+});
